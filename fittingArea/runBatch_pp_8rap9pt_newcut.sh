0c36f812ae659db40e2d7fca8073ae4b5ff4d708
SongkyoLee <songkyo.lee@cern.ch>
2016-05-04 14:21:02 +0200

    8rap9pt new cut

--- fittingArea/runBatch_pp_8rap9pt_newcut.sh
+++ fittingArea/runBatch_pp_8rap9pt_newcut.sh
@@ -31,7 +31,7 @@ prmc=/afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/out
 npmc=/afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_NPMC_pp_newcut_off/outRoo_NPMC_pp_newcut_off.root
 
 ########### options
-sysString="nominal" ## sys01_01-05, sys02_01, sys03_01-02, sys04_01
+sysString="nominal" ## sys01_01,...,sys01_05, sys02_01, sys03_01, sys03_02, sys04_01
 mcweight=0  #0: Do NOT mcweight(dataJpsi), 1: Do mcweight(dataJpsiWeight)
 dataMerge=1 # number of input data files to be merged
 mcMerge=1 # number of input mc files to be merged
@@ -39,8 +39,9 @@ isPA=0 # 0:pp, 1:Pbp, 2:pPb, 3:pAMerged
 eventActivity=0 #0:nothing 1:Ntrack 2:ET^{HF}
 absoluteY=0 #use absolute y binning or not (e.g. 1.6 < |y| < 2.4)
 ### read the ctauErrFile or not
-readct=0 #0:calculate in the code, 1:read from file, 2:constant
-cterrfile=$(pwd)/outCtErr/fit_ctauErrorRange_pPb.txt
+#readct=0 #0:calculate in the code, 1:read from file, 2:constant
+readct=1 #0:calculate in the code, 1:read from file, 2:constant
+cterrfile=$(pwd)/outCtErr/fit_ctauErrorRange_pp_8rap9pt_newcut.txt
 
 #mSigF="G1CB1Sig" # Mass signal function name (options: sigCB2WNG1 (default), signalCB3WN)
 #mBkgF="expBkg" # Mass background function name (options: expFunct (default), polFunct)
@@ -87,22 +88,27 @@ function program {
   printf "tar zcvf %s.tgz %s*\n" $work $work >> $scripts/$work.sh
   printf "cp %s.tgz %s\n" $work $storage >> $scripts/$work.sh
   printf "rm -f %s*\n" $work >> $scripts/$work.sh #for_LXPLUS (batch)
-	#$(pwd)/condor_executable_simple.sh	$work #for_KUNPL (condor)
 	bsub -R "pool>10000" -u songkyo.lee@cer.c -q 1nd -J $work < $scripts/$work.sh #for_LXPLUS (batch)
+	#$(pwd)/condor_executable_simple.sh	$work #for_KUNPL (condor)
 }
 
 ################################################################ 
 ########## Running script with pre-defined binnings
 ################################################################ 
 
-#for rap in ${rapbins[@]}; do
-#	for pt in ${ptbins[@]}; do
-#		program $rap $pt $ntrkbins $ethfbins
-#	done
-#done
-#program -2.40--1.93 2.0-3.0 $ntrkbins $ethfbins 
-#program 1.93-2.40 2.0-3.0 $ntrkbins $ethfbins 
+for rap in ${rapbins[@]}; do
+	for pt in ${ptbins[@]}; do
+		program $rap $pt $ntrkbins $ethfbins
+	done
+done
+program -2.40--1.93 2.0-3.0 $ntrkbins $ethfbins 
+program 1.93-2.40 2.0-3.0 $ntrkbins $ethfbins 
 
 ### TEST
-program -0.90--0.00 10.0-30.0 $ntrkbins $ethfbins 
+#program -2.40--1.93 2.0-3.0 $ntrkbins $ethfbins 
+#program -2.40--1.93 5.0-6.5 $ntrkbins $ethfbins 
+#program -2.40--1.93 8.5-10.0 $ntrkbins $ethfbins 
+#program 0.90-1.50 8.5-10.0 $ntrkbins $ethfbins 
+#program 1.93-2.40 2.0-3.0 $ntrkbins $ethfbins 
+#program 1.93-2.40 14.0-30.0 $ntrkbins $ethfbins 
 
