a808ed64eb1034dcc47a27316e6a35cff8b6a7b3
SongkyoLee <songkyo.lee@cern.ch>
2016-05-04 14:21:45 +0200

    8rap9pt new cut

--- fittingArea/run.sh
+++ fittingArea/run.sh
@@ -2,12 +2,18 @@
 #################################################
 ############ for pp 
 #################################################
-./runBatch_pp.sh Fit2DData /afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_Data_pp_newcut/outRoo_Data_pp_newcut.root pp_8rap9pt_newcut >& log_pp_8rap9pt_newcut &  
+### 8rap9pt newcut
+#./runBatch8rap9pt_pp.sh Fit2DData /afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_Data_pp_newcut/outRoo_Data_pp_newcut.root pp_8rap9pt_newcut >& log_pp_8rap9pt_newcut &  
+### 8rap9pt oldcut
+#./runBatch8rap9pt_pp.sh Fit2DData /afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_Data_pp_oldcut/outRoo_Data_pp_oldcut.root pp_8rap9pt_oldcut >& log_pp_8rap9pt_oldcut &  
 
 #################################################
 ############ for pA (Pbp + pPbFlip)
 #################################################
-./runBatch_pAMerged.sh Fit2DData /afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_Data_Pbp_newcut_v1/outRoo_Data_Pbp_newcut_v1.root /afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_Data_Pbp_newcut_v2/outRoo_Data_Pbp_newcut_v2.root /afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_Data_pPbFlip_newcut/outRoo_Data_pPbFlip_newcut.root pAMerged_8rap9pt_newcut >& log_pAMerged_8rap9pt_newcut & 
+### 8rap9pt newcut
+./runBatch8rap9pt_pA.sh Fit2DData /afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_Data_Pbp_newcut_v1/outRoo_Data_Pbp_newcut_v1.root /afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_Data_Pbp_newcut_v2/outRoo_Data_Pbp_newcut_v2.root /afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_Data_pPbFlip_newcut/outRoo_Data_pPbFlip_newcut.root pA_8rap9pt_newcut >& log_pA_8rap9pt_newcut & 
+### 8rap9pt oldcut
+#./runBatch8rap9pt_pA.sh Fit2DData /afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_Data_Pbp_oldcut_v1/outRoo_Data_Pbp_oldcut_v1.root /afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_Data_Pbp_oldcut_v2/outRoo_Data_Pbp_oldcut_v2.root /afs/cern.ch/work/k/kyolee/private/CMSSW_8_0_0/src/2Dfitting/rooDataSet/outRoo_Data_pPbFlip_oldcut/outRoo_Data_pPbFlip_oldcut.root pA_8rap9pt_oldcut >& log_pA_8rap9pt_oldcut & 
 
 
 
