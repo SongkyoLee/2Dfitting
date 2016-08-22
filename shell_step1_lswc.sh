b75078901e2382aed84623236660761a41af81b6
SongkyoLee <songkyo.lee@cern.ch>
2016-08-22 14:10:41 +0200

    shell scripts to collect results files in a single directory (for scp)

---
+++ shell_step1_lswc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sysbins=(nominal sys01_01 sys01_02 sys01_03 sys02_01 sys03_01 sys03_02 sys04_01)
+binNameArr=("pp_8rap9pt_newcut" "pA_8rap9pt_newcut" "pA_6rap2pt_newcut")
+
+for binName in ${binNameArr[@]};
+do
+  for isys in ${sysbins[@]};
+  do
+    echo "dir :: "$binName"_"$isys
+    ls fittingArea/Results/$binName"_"$isys | wc
+    ls fittingArea/Results/$binName"_"$isys | wc
+  done
+done
+
---
+++ shell_step3_collectTgz.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+  
+if [ ! -d "$(pwd)/tmpTgz" ]; then
+  mkdir $(pwd)/tmpTgz
+fi
+
+sysbins=(nominal sys01_01 sys01_02 sys01_03 sys02_01 sys03_01 sys03_02 sys04_01)
+binNameArr=("pp_8rap9pt_newcut" "pA_8rap9pt_newcut" "pA_6rap2pt_newcut")
+
+for binName in ${binNameArr[@]};
+do
+  for isys in ${sysbins[@]};
+  do
+    echo "dir :: "$binName"_"$isys
+    cp -r fittingArea/Results/tar_$binName"_"$isys".tgz" tmpTgz/tar_$binName"_"$isys".tgz"
+  done
+done
